eed24255b051f64092278d33aa3ffdca2e5b295e
Viniciusxz <viniciusribeirolinsr@gmail.com>
2026-05-14 23:45:54 -0300

    Criando as routes de titulo.

--- src/modules/planos/plano.routes.js
+++ src/modules/planos/plano.routes.js
@@ -1,1 +1,13 @@
 // plano de estudo routes
+import { Router } from "express";
+import planoController from "./plano.controller.js";
+
+const router = Router();
+
+router.get("/titulo", planoController.pegaTodos);
+router.get("/titulo/:id", planoController.pegaUm);
+router.post('/titulo', planoController.criarUm);
+router.put('/titulo/:id', planoController.atualizarUm);
+router.delete('/titulo/:id', planoController.excluirUm);
+
+export default router;
