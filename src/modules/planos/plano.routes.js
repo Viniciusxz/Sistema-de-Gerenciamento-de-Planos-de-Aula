98c7181bfcbe5f5e137757da14c0f348947e87c9
Viniciusxz <viniciusribeirolinsr@gmail.com>
2026-05-16 12:06:54 -0300

    feat: correção de sintaxe.

--- src/modules/planos/plano.routes.js
+++ src/modules/planos/plano.routes.js
@@ -1,14 +1,14 @@
 // plano de estudo routes
 import { Router } from "express";
 import planoController from "./plano.controller.js";
-import {  validarCriarPlano, validarId } from "../../middlewares/planos.middleware.js";
+import validar from "../../middlewares/planos.middleware.js";
 
 const router = Router();
 
 router.get("/", planoController.pegaTodos);
-router.get("/:id", validarId, planoController.pegaUm);
-router.post('/', validarCriarPlano, planoController.criarUm);
-router.put('/:id', validarId, validarCriarPlano, planoController.atualizarUm);
-router.delete('/:id', validarId, planoController.excluirUm);
+router.get("/:id", validar.validarId, planoController.pegaUm);
+router.post('/', validar.validarCriarPlano, planoController.criarUm);
+router.put('/:id', validar.validarId, validar.validarCriarPlano, planoController.atualizarUm);
+router.delete('/:id', validar.validarId, planoController.excluirUm);
 
 export default router;
